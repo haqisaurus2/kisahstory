4b3669437a89389acf5b4661578bb7a4b81fbdd5
Imam Baehaqi <haqisaurus@gmail.com>
2024-08-10 13:17:16 +0700

    table name

--- app/Models/ComicChapterComment.php
+++ app/Models/ComicChapterComment.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ComicChapterComment extends Model
 {
     use HasFactory;
+    protected $table = 'story_chapter_comments';
     protected $fillable = ['user_id', 'chapter_id', 'parent_id', 'body'];
     public function user() {
         return $this->belongsTo(User::class);
