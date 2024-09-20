7a5cb9a95a66dd763ba936f6684065eedf7e267f
Imam Baehaqi <haqisaurus@gmail.com>
2024-09-20 13:38:49 +0700

    - update tag

--- resources/views/sitemap-story.blade.php
+++ resources/views/sitemap-story.blade.php
@@ -1,23 +1,25 @@
 {!! $xml_version !!}
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>{{url('/story/' . $story->slug)}}</loc>
-            <lastmod>{{\Carbon\Carbon::parse(time: $story->updated_at)->setTimezone('Asia/Jakarta')->format('Y-m-d\TH:i:sP')}}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.8</priority>
-        </url>
-        @foreach($story->chapters as $chapter)
+<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>{{url('/story/' . $story->slug)}}</loc>
+        <lastmod>
+            {{\Carbon\Carbon::parse(time: $story->updated_at)->setTimezone('Asia/Jakarta')->format('Y-m-d\TH:i:sP')}}
+        </lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    @foreach($story->chapters as $chapter)
         <url>
             <loc>{{url('/chapter/' . $chapter->slug)}}</loc>
-            <lastmod>{{date_format(date_create($chapter->updated_at),"YYYY-MM-DDThh:mm:ss.sTZD")}}</lastmod>
-            <lastmod>{{\Carbon\Carbon::parse(time: $chapter->updated_at)->setTimezone('Asia/Jakarta')->format('Y-m-d\TH:i:sP')}}</lastmod>
+            <lastmod>{{date_format(date_create($chapter->updated_at), "YYYY-MM-DDThh:mm:ss.sTZD")}}</lastmod>
+            <lastmod>
+                {{\Carbon\Carbon::parse(time: $chapter->updated_at)->setTimezone('Asia/Jakarta')->format('Y-m-d\TH:i:sP')}}
+            </lastmod>
 
             <changefreq>monthly</changefreq>
             <priority>0.7</priority>
         </url>
-        @endforeach
-    </urlset>
- 
+    @endforeach
 </urlset>
--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,5 +1,7 @@
 {!! $xml_version !!}
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd"
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     {{--
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
@@ -8,28 +10,29 @@
             <changefreq>daily</changefreq>
             <priority>1.0</priority>
         </url>
-        @foreach($stories as $story) 
-                <url>
-                    <loc>{{url('/story/' . $story->slug)}}</loc>
-                    <lastmod>{{date('YYYY-MM-DDThh:mmTZD')}}</lastmod>
-                    <changefreq>weekly</changefreq>
-                    <priority>0.8</priority>
-                </url>
-                @foreach($story->chapters as $chapter)
-                <url>
-                    <loc>{{url('/chapter/' . $chapter->slug)}}</loc>
-                    <lastmod>{{date_format(date_create($chapter->updated_at),"YYYY-MM-DDThh:mmTZD")}}</lastmod>
-                    <changefreq>monthly</changefreq>
-                    <priority>0.7</priority>
-                </url>
-                @endforeach 
+        @foreach($stories as $story)
+        <url>
+            <loc>{{url('/story/' . $story->slug)}}</loc>
+            <lastmod>{{date('YYYY-MM-DDThh:mmTZD')}}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+        @foreach($story->chapters as $chapter)
+        <url>
+            <loc>{{url('/chapter/' . $chapter->slug)}}</loc>
+            <lastmod>{{date_format(date_create($chapter->updated_at),"YYYY-MM-DDThh:mmTZD")}}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>0.7</priority>
+        </url>
+        @endforeach
         @endforeach
     </urlset>
     --}}
     @foreach($stories as $story)
         <sitemap>
             <loc>{{url('/')}}/sitemap-{{$story->slug}}.xml</loc>
-            <lastmod>{{\Carbon\Carbon::parse($story->updated_at)->setTimezone('Asia/Jakarta')->format('Y-m-d\TH:i:sP')}}</lastmod>
+            <lastmod>{{\Carbon\Carbon::parse($story->updated_at)->setTimezone('Asia/Jakarta')->format('Y-m-d\TH:i:sP')}}
+            </lastmod>
         </sitemap>
     @endforeach
 </sitemapindex>
