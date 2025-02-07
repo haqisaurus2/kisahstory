8ade7903ad2996506bc26d95e198988fa39be043
Imam Baehaqi <haqisaurus@gmail.com>
2025-02-07 23:32:01 +0700

    login redirect

--- resources/react/Login.tsx
+++ resources/react/Login.tsx
@@ -16,7 +16,11 @@ const Login: React.FC = () => {
             loginUsingGoogle(tokenId)
                 .then((response: any) => {
                     auth.login(response.access_token, response.expires_in);
-                    navigate('/dashboard/');
+                    if (auth.user.google_id=='111278741725984561867') {
+                        navigate("/admin");
+                    } else {
+                        navigate('/dashboard/');
+                    }
                 })
                 .finally(() => {
                 })
